313e975029d4a8c01b15c6b90c49af2abcc2ff3f
Mario Delgado <mario.c.delgado@gmail.com>
2014-06-02 22:03:49 -0700

    initial

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,50 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+var myVariable = 42
+
+myVariable = 50
+
+let myConstant = 42
+
+let implcitInterger = 70
+
+let explicitDouble: Double = 70
+
+let explicitFloat: Float = 4
+
+let label = "The width is "
+
+let width = 94
+
+let widthLabel = label + String(width)
+
+let apples = 3
+
+let oranges = 5
+
+let appleSummary = "I have \(apples) apples."
+
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+let name = "Mario"
+
+let helloMario = "Hello there \(name)!"
+
+
+
+var shoppingList = ["catfish", "water", "tulips"]
+    shoppingList[1] = "bottle of water"
+
+var occupations = [
+"Malcolm": "Captain",
+"Kaylee": "Mechanic",]
+
+occupations["Jayne"] = "Public Relations"
+
+let emptyArray = String[]()
+let emptyDictionary = Dictionary<String, Float>()
+
